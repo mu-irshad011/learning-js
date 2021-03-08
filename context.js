8b057ccc1659f253bcbbf2ff6f3e5962290ab9c0
irshad <smoirshad11@gmail.com>
2021-03-08 22:50:40 +0530

    second commit

--- context.js
+++ context.js
@@ -3,9 +3,10 @@ const name = "Dixit"
 const funName = () => {
     const name = "irshad"
     console.log(name)
+    console.log(name)
 }
 
 funName()
 
-// global >>> fun, outside fun
-// fun >>> fun
+// global >>> fun, outside fund
+// fun >>> fundf 
